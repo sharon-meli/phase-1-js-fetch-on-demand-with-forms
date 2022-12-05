71ae647029a202a34194b32eccdb4368d28aeae9
sharon <sharonchepkirui665@gmail.com>
2022-12-05 23:58:57 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    const input = document.querySelector('input#searchByID');
+  
+  console.log(input.value);fetch('http://localhost:3000/movies')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);const title = document.querySelector('section#movieDetails h4');
+    const summary = document.querySelector('section#movieDetails p');
+
+    title.innerText = data.title;
+    summary.innerText = data.summary;
+
+  });
+
+ });
   
 }
 
